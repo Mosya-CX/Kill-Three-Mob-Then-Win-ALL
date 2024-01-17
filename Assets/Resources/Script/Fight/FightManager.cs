b953ad8bb7eed6e5f364da531e08c7fafed0611f
Tr8ceur <3381799956@qq.com>
2024-01-17 20:14:59 +0800

    FightManager战斗管理器(初步添加)

---
+++ Assets/Resources/Script/Fight/FightManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//战斗枚举
+public enum FightType
+{
+    None,
+    Init,
+    Player,//玩家回合
+    Enemy,//敌人回合
+    Win,
+    Lose
+}
+
+/// <summary>
+/// 战斗管理器
+/// </summary>
+public class FightManager : MonoBehaviour
+{
+    public static FightManager instance;
+
+    public FightUnit fightUnit;//战斗单元
+
+    private void Awake()
+    { 
+        instance = this; 
+    }
+
+    //切换战斗类型
+    public void ChangeType(FightType type)
+    {
+        switch (type) 
+        {
+            case FightType.None:
+                break;
+            case FightType.Init:
+                fightUnit = new FightUnit();
+                break;
+            case FightType.Player:
+                fightUnit = new Fight_PlayerTurn();
+                break;
+            case FightType.Enemy:
+                fightUnit = new Fight_EnemyTurn();
+                break;
+            case FightType.Win:
+                fightUnit = new Fight_Win();
+                break;
+            case FightType.Lose:
+                fightUnit = new Fight_Lose();
+                break;
+        }
+        fightUnit.Init();//初始化
+    }
+
+    private void Update()
+    {
+        if(fightUnit != null) 
+        {
+            fightUnit.OnUpdate();
+        }
+    }
+}
