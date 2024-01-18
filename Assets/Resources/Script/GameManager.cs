9601a0e5c6a9912b4c331cdce1ea3fb2bab4b568
Tr8ceur <3381799956@qq.com>
2024-01-18 15:37:56 +0800

    音效管理器的加入

--- Assets/Resources/Script/GameManager.cs
+++ Assets/Resources/Script/GameManager.cs
@@ -9,18 +9,30 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance = new GameManager();
     public int currentProgress;
     public bool isFighting;
+    private bool fightBGMIsOn = false;
     public GameObject Player;
     public GameObject Enemy;
 
     //public int currentTurn;// 0表示非战斗状态，1表示玩家回合，2表示敌人回合
     private void Awake()
     {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         DontDestroyOnLoad(gameObject);
     }
     private void Start()
     {
+        //播放开局bgm
+        AudioManager.Instance.StartLevelAudio();
+
         Player = GameObject.FindWithTag("Player");
         Enemy = GameObject.FindWithTag("Enemy");
+
+
         // 初始化进度
         currentProgress = 0;
         //// 初始化战斗回合
@@ -39,6 +51,12 @@ public class GameManager : MonoBehaviour
         if (isFighting)
         {
             
+        }
+        //进入战斗播放战斗BGM
+        if (isFighting && !fightBGMIsOn)
+        {
+            AudioManager.Instance.FightingAudio();
+            fightBGMIsOn = true;
         }
         switch (currentProgress)
         {
