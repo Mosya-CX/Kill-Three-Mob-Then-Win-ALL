ca9588b6842cf5ddd084a91e9bb97acdc457f283
Tr8ceur <3381799956@qq.com>
2024-01-19 00:39:59 +0800

    backup

--- Assets/Resources/Script/Card/CardItem.cs
+++ Assets/Resources/Script/Card/CardItem.cs
@@ -10,4 +10,31 @@ public class CardItem : MonoBehaviour
     { 
         this.data = data; 
     }
+
+    //尝试使用卡牌
+    public virtual bool TryUse()
+    {
+        //卡牌需要的费用
+        int cost = int.Parse(data["Expend"]);
+
+        if(cost > FightManager.Instance.curFee)
+        {
+            //费用不足
+            //是否加音效待定
+
+            //提示
+            //UIManager.Instance.ShowTip("费用不足",Color.red);
+
+            return false;
+        }
+        else
+        {
+            //减少目前费用
+            FightManager.Instance.curFee -= cost;
+            //刷新费用文本
+            //UIManager.Instance.GetUI<FightUI>("FightUI").UpdateFee();
+
+            return true;
+        }
+    }
 }
