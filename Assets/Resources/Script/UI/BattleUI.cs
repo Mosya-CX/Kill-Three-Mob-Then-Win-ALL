db3c039b1de6c09ca8adf740c0f602da58222384
bear sur <liu20041218@outlook.com>
2024-01-19 20:19:52 +0800

    Update BattleUI.cs

--- Assets/Resources/Script/UI/BattleUI.cs
+++ Assets/Resources/Script/UI/BattleUI.cs
@@ -6,15 +6,5 @@ using UnityEngine.UI;
 
 public class BattleUI : MonoBehaviour
 {
-    private Text playerHPText;    //玩家血量显示
-    private Image playerHealthImage;     //血条
-    private Text playerShieldText;   //护盾值
-    private Text cardCountText;   //现有手牌数量
-    private Text usedCardCountText;    //弃牌堆数量
-    private void Awake()
-    {
-        playerHealthImage = transform.Find("playerHP/playerHP_Image").GetComponent<Image>();
-        playerHPText = transform.Find("playerHP/playerHPText").GetComponent<Text>();    
-        playerShieldText = transform.Find("Shield/playerShieldText").GetComponent <Text>();
-    }
+   
 }
