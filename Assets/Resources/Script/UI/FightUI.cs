0888f1f48bd4a8b083b8e161e2b36d292557db2c
Tr8ceur <3381799956@qq.com>
2024-01-19 22:59:45 +0800

    backup

--- Assets/Resources/Script/UI/FightUI.cs
+++ Assets/Resources/Script/UI/FightUI.cs
@@ -14,12 +14,50 @@ public class FightUI : BasePanel
 
     private void Awake()
     {
-        cardNumTxt = transform.Find("Card/CardNumText").GetComponent<Text>();
-        usedCardNumTxt = transform.Find("UsedCard/UsedCardNumText").GetComponent<Text>();
-        feeTxt = transform.Find("Fee/FeeCountText").GetComponent<Text>();
-        hpTxt = transform.Find("playerHP/playerHPText").GetComponent<Text>();
-        hpImg = transform.Find("playerHP/playerHP_Image").GetComponent<Image>();
-        defenseTxt = transform.Find("Shield/playerShieldText").GetComponent<Text>();
+        cardNumTxt = transform.Find("hasCard/icon/Text").GetComponent<Text>();
+        usedCardNumTxt = transform.Find("noCard/icon/Text").GetComponent<Text>();
+        feeTxt = transform.Find("mana/Text").GetComponent<Text>();
+        hpTxt = transform.Find("hp/hpText").GetComponent<Text>();
+        hpImg = transform.Find("hp/fill").GetComponent<Image>();
+        defenseTxt = transform.Find("hp/fangyu/Text").GetComponent<Text>();
 
+    }
+    private void Start()
+    {
+        UpdateHp();
+        UpdateFee();
+        UpdateDefense();
+        UpdateCardNum();
+        updateUsedCardNum();
+    }
+    //更新血量显示
+    public void UpdateHp()
+    {
+        hpTxt.text = FightManager.Instance.curHp + "/" + FightManager.Instance.maxHp;
+        hpImg.fillAmount = (float)FightManager.Instance.curHp / (float)FightManager.Instance.maxHp;
+    }
+
+    //更新费用
+    public void UpdateFee()
+    {
+        feeTxt.text = FightManager.Instance.curFee + "/" + FightManager.Instance.maxFee;
+    }
+
+    //更新防御值
+    public void UpdateDefense()
+    {
+        defenseTxt.text = FightManager.Instance.defenseCount.ToString();
+    }
+
+    //更新卡堆数量
+    public void UpdateCardNum()
+    {
+        cardNumTxt.text = FightCardManager.instance.availableCardList.Count.ToString();
+    }
+
+    //更新弃牌堆数量
+    public void updateUsedCardNum()
+    {
+        usedCardNumTxt.text = FightCardManager.instance.usedCardList.Count.ToString();
     }
 }
