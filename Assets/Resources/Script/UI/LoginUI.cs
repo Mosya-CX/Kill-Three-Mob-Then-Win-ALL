75357d512c6ca72f1d40c5e2a18923a93a09b7b3
Tr8ceur <3381799956@qq.com>
2024-01-21 00:37:08 +0800

    1

--- Assets/Resources/Script/UI/LoginUI.cs
+++ Assets/Resources/Script/UI/LoginUI.cs
@@ -8,19 +8,29 @@ using UnityEngine.UI;
 public class LoginUI : BasePanel
 {
     public Button startBtn;
+    public Button quitBtn;
     private void Awake()
     {
         //开始游戏
         //Register("bg/startBin").onClick = onStartGameBin;
-        startBtn.onClick.AddListener(onStartGameBin);
+        startBtn.onClick.AddListener(onStartGameBtn);
+        quitBtn.onClick.AddListener(onQuitGameBtn);
     }
-
-    private void onStartGameBin() 
+    //开始游戏
+    private void onStartGameBtn() 
     {
         //战斗初始化
         FightManager.Instance.ChangeType(FightType.Init);
         //关闭页面
         Debug.Log("关闭页面");
         Destroy(gameObject);
+    }
+    //退出游戏
+    private void onQuitGameBtn()
+    {
+        //打包后才会生效
+        Application.Quit();
+        //编辑器内可用下面这行
+        //UnityEditor.EditorApplication.isPlaying =false;
     }
 }
